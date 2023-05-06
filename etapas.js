4f1f1c05fd443136b5f16c77ad522a75e772e6b4
Gustavo Neves <129140834+Sevengusta@users.noreply.github.com>
2023-05-05 22:58:42 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Xaveco',
+                partido: 'PTX - Partido dos Bons Xavequeiros',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Do contra',
+                partido: 'PTA - Partido do Tudo ao Contrário',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Titi',
+                partido: 'PCC - Partido dos Criativos Comilões',
+                vice: 'Magali',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Titi',
+                partido: 'PBA - Partido dos Bons Amigos',
+                vice: 'Mônica',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
